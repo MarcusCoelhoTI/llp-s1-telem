fb54114f424c5c4d13286a286038db55ffeeec68
Marcus <mvsc20202gmail.com>
2024-04-09 17:09:28 -0300

    fiz atividade 02

--- atividades/atividade-02/atividade-02.c
+++ atividades/atividade-02/atividade-02.c
@@ -1,3 +1,11 @@
+/*Atividade-02
+Faça um programa usando a linguagem C que realiza a seguinte tarefa:
+1) Use uma estrutura de repetição para mostrar na saída números inteiros pares entre 1 e 50.
+
+Pode ser feito em Dupla. 
+Mas deve ser registrado no GitHub de cada aluno envolvido.
+Para evidenciar a entrega da atividade no Classroom deve ser postado o link da Atividade-02 no Github.*/
+
 #include <stdio.h>
 
 int main (void){
